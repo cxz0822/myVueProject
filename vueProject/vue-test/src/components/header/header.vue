fc646836996dd0653919c745ae979dfd5a71f976
cxz0822 <593319431@qq.com>
2017-04-24 18:35:01 +0800

    网站头部

---
+++ vueProject/vue-test/src/components/header/header.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="app" class="header">
+        <div class="header-top">
+            <div class="header-top-wrap clearfix">
+                <div class="left">传智播客旗下高端在线IT职业教育学习平台</div>
+                <div class="right">全国咨询热线：400-618-4000</div>
+            </div>
+        </div>
+        <div class="header-middle">
+            <div class="header-middle-bg clearfix">
+                <div class="left">
+                    <img src="../../assets/images/index/logo.png" alt=""/><span>黑马程序员线上平台</span>
+                </div>
+                <div class="right">
+                    <router-link to="">学习中心</router-link>
+                    <router-link to="">购物车</router-link>
+                    <div class="loginAndRegister">
+                        <el-dropdown>
+                    <span class="el-dropdown-link">
+                        <router-link to="">登陆</router-link>
+                        <span class="separator">|</span>
+                        <router-link to="">注册</router-link><i class="el-icon-caret-bottom el-icon--right"></i>
+                    </span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item>黄金糕</el-dropdown-item>
+                                <el-dropdown-item>狮子头</el-dropdown-item>
+                                <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                                <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                                <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-bottom">
+            <div class="header-bottom-wrap clearfix">
+                <div class="allSubjects">全部学科</div>
+                <ul class="courseType clearfix">
+                    <li class="active">首页</li>
+                    <li>就业课</li>
+                    <li>
+                        <div class="microMajor">
+                            <el-dropdown>
+                                <span class="el-dropdown-link">微专业<i class="el-icon-caret-bottom el-icon--right"></i></span>
+                                <el-dropdown-menu slot="dropdown">
+                                    <el-dropdown-item>黄金糕</el-dropdown-item>
+                                    <el-dropdown-item>狮子头</el-dropdown-item>
+                                    <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                                    <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                                    <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                                </el-dropdown-menu>
+                            </el-dropdown>
+                        </div>
+                    </li>
+                    <li>微课堂</li>
+                    <li>免费课程</li>
+                    <li>问答精灵</li>
+                    <li>博学社</li>
+                    <li>学习社区</li>
+                </ul>
+                <div class="searchBox">
+                    <input type="text" placeholder="搜索课程" class="searchInput"/>
+                    <i class="el-icon-search searchBtn"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'header'
+    }
+</script>
+
+<style scoped rel="stylesheet/less" lang="less">
+    @import "header.less";
+</style>
--- vueProject/vue-test/src/main.js
+++ vueProject/vue-test/src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
-import App from './App'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+/**--ElementUI按需加载- start-*/
+Vue.use(Element);
+
+/**--ElementUI按需加载- end-*/
+import Header from '../src/components/header/header'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+const app=new Vue({
   el: '#app',
   router,
-  render: h => h(App)
-})
+  render: h => h(Header)
+});
+app.$mount('#app');
